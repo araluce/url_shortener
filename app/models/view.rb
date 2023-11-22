90a1964c9b6db32f5c142d38772040607fca6594
araluce <alvaro.fernandez@crisalix.com>
2023-11-22 20:12:03 +0100

    Fix MetadataView trigger

--- app/models/view.rb
+++ app/models/view.rb
@@ -1,7 +1,5 @@
 class View < ApplicationRecord
   belongs_to :link, counter_cache: true
 
-  after_save_commit if: :user_agent_changed? do
-    MetadataViewJob.perform_later(id)
-  end
+  after_save_commit -> { MetadataViewJob.perform_later(id) }
 end
